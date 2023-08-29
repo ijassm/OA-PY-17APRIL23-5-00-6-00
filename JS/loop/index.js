6e0dc0c94a19322e43a61a2a3d68f002b3d1d2e4
ijassm <ijass.ocean@gmail.com>
2023-08-29 18:11:03 +0530

    for loop - 29 AUG 2023

---
+++ JS/loop/index.js
@@ -0,0 +1,18 @@
+// for loop
+
+const arr = [128000, 128001, 128002, 128003, 128004];
+
+const object = {
+  blackRat: 128000,
+  whiteRat: 128001,
+  yak: 128002,
+  buffalo: 128003,
+};
+
+Array.prototype.a = "string";
+
+// for (let i = 128000; i <= 129510; i++) document.write(`<h1>&#${i};</h1>`);
+
+for (const i of arr) document.write(`<h1>&#${i};</h1>`);
+
+// for (const i in object) document.write(`<h1>&#${object[i]};</h1>`);
