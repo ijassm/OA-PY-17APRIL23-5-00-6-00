5b7d8bc28241caab2dda4cbb4b1a60b0842d123a
ijassm <ijass.ocean@gmail.com>
2023-08-30 18:14:16 +0530

    for while loop - 30 AUG 2023

---
+++ JS/for-loop/index.js
@@ -0,0 +1,67 @@
+// for loop
+
+// const arr = [128000, 128001, 128002, 128003, 128004];
+
+// const object = {
+//   blackRat: 128000,
+//   whiteRat: 128001,
+//   yak: 128002,
+//   buffalo: 128003,
+// };
+
+// Array.prototype.a = "string";
+
+// for (let i = 128000; i <= 129510; i++) document.write(`<h1>&#${i};</h1>`);
+
+// for (const i of arr) document.write(`<h1>&#${i};</h1>`);
+
+// for (const i in object) document.write(`<h1>&#${object[i]};</h1>`);
+
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 2; j++) {
+    for (let k = 1; k <= 2; k++) {
+      console.log(i, j, k);
+    }
+  }
+}
+
+// i = 1
+//     j = 1 | j =2
+// i = 2
+//     j = 1 | j =2
+// i = 3
+//     j = 1 | j =2
+// i = 4
+//     j = 1 | j =2
+// i = 5
+//     j = 1 | j =2
+
+// i = 1
+//     j = 1
+//          k = 1 | k = 2
+//     j = 2
+//          k = 1 | k = 2
+
+// i = 2
+//     j = 1
+//          k = 1 | k = 2
+//     j = 2
+//          k = 1 | k = 2
+
+// i = 3
+//     j = 1
+//          k = 1 | k = 2
+//     j = 2
+//          k = 1 | k = 2
+
+// i = 4
+//     j = 1
+//          k = 1 | k = 2
+//     j = 2
+//          k = 1 | k = 2
+
+// i = 5
+//     j = 1
+//          k = 1 | k = 2
+//     j = 2
+//          k = 1 | k = 2
