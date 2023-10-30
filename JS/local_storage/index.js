71497b95429543f078215bd88d0d2eeeba254750
ijassm <ijass.ocean@gmail.com>
2023-10-30 18:39:41 +0530

    30 OCT 2023

---
+++ JS/local_storage/index.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "John",
+  age: 34,
+};
+
+// localStorage.setItem("obj4", JSON.stringify(person));
+// console.log(typeof JSON.parse(localStorage.getItem("obj")));
+// console.log(JSON.parse(localStorage.getItem("obj")));
+// console.log(localStorage.getItem("obj").name);
+
+// console.log(localStorage.key(2));
+
+// localStorage.removeItem("obj");
+localStorage.clear();
